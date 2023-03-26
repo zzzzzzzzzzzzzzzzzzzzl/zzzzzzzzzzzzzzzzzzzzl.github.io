dc0f58bc920ff86daac20d3879c65146c3d71bd3
zzzzzzzzzzzzzzzzzzzzl <james.edwards5566@gmail.com>
2023-03-26 20:54:20 +1300

    shaved some stuff off

--- headerdiv_for_folder.js
+++ headerdiv_for_folder.js
@@ -19,38 +19,6 @@ var html=`<div class="header">
     
 
 
-        <!-- tech -->
-        <button class="dropbtn">Tech</button> 
-        <div class="dropdown-content">
-        <a href="${s}tech/1.html">Inline vs Inline-block</a>
-        <a href="${s}tech/2.html">HTML,CSS and JS</a>
-        <a href="${s}tech/3.html">Code readability</a>
-        <a href="${s}tech/4.html">Problem solving</a>
-        </div>
-      </div>
-
-        <div class="dropdown">
-            <!-- core -->
-            <button class="dropbtn">Core</button>
-            <div class="dropdown-content">
-              <a href="${s}core/1.html">The Story of Te Houtaewa</a>
-              <a href="${s}core/2.html">Learning plan</a>
-              <a href="${s}core/3.html">Values</a>
-              <a href="${s}core/4.html">Emotional Inteligence</a>
-            </div>
-          </div>
-          
-            <div class="dropdown">
-                <!-- sprints -->
-                <button class="dropbtn">Sprints</button>
-                <div class="dropdown-content">
-                <a href="${s}sprints/1.html">Week 1</a>
-                <a href="${s}sprints/2.html">Week 2</a>
-                <a href="${s}sprints/3.html">Week 3</a>
-                <a href="${s}sprints/4.html">Week 4</a>
-
-                </div>
-              </div>
 
 
               <div class="dropdown">
